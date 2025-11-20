8a82e84de50349adaeccc816c34e94b7fc66343c
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2025-11-20 02:26:03 +0000

    Initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set nu")
+vim.cmd("set ic")
+vim.cmd("let matchparen_disable_cursor_hl = 1")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
